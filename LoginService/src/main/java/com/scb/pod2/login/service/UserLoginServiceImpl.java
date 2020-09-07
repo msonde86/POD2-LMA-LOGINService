4a0d948beabd6c65738f7e5ca5a27e9c0b4aa541
unknown <ctsjavauser298@iiht.tech>
2020-09-07 10:20:15 +0530

    Password encryption as shared by somak

--- LoginService/src/main/java/com/scb/pod2/login/service/UserLoginServiceImpl.java
+++ LoginService/src/main/java/com/scb/pod2/login/service/UserLoginServiceImpl.java
@@ -1,21 +1,19 @@
 package com.scb.pod2.login.service;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.scb.pod2.login.dao.UserLoginResource;
-import com.scb.pod2.login.dto.UserDTO;
 import com.scb.pod2.login.model.User;
+import com.scb.pod2.login.util.PasswordDataEncryption;
 
 @Service
 public class UserLoginServiceImpl implements UserLoginService {
 
 	@Autowired
 	private UserLoginResource loginResource;
-	
+	@Autowired
+	private PasswordDataEncryption encryption;
 	/*
 	 * Encode the password using base 64.
 	 * Check if the given credentials is present in DB
@@ -25,17 +23,11 @@ public class UserLoginServiceImpl implements UserLoginService {
 		User loginUser = loginResource.findUser(user.getEmailId(), encodePassword(user.getPassword()));
 		return loginUser;
 	}
-
-	@Override
-	public UserDTO setUserDTO(User user) {
-		UserDTO userDTO = new UserDTO();
-		userDTO.setUserId(user.getUserId());
-		userDTO.setUserName(user.getEmailId());
-		return userDTO;
-	}
+	
 
 	public String encodePassword(String password) {
-		return Base64.getUrlEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8));
+		//return Base64.getUrlEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8));
+		return encryption.encryptPassword(password, "scbpod2");
 	}
 
 }
